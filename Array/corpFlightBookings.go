7a9d868fe56be7616ddc471b477fb5ba63edd2cc
shirnyQi <34259132+shirnyQi@users.noreply.github.com>
2021-07-11 15:08:49 +0800

    Create corpFlightBookings.go

---
+++ Array/corpFlightBookings.go
@@ -0,0 +1,22 @@
+/*
+lc:https://leetcode-cn.com/problems/corporate-flight-bookings/
+*/
+
+func corpFlightBookings(bookings [][]int, n int) []int {
+	var counter = make([]int, n, n)
+	for _, booking := range bookings {
+		counter[booking[0]-1] += booking[2]
+		if booking[1] < n {
+			counter[booking[1]] -= booking[2]
+		}
+	}
+
+	for i := range counter {
+		if i == 0 {
+			continue
+		}
+		counter[i] += counter[i-1]
+	}
+
+	return counter
+}
