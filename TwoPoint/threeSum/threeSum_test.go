46a466b0cc833fe62dddde16e6db0c6a6fa09162
shirnyQi <846934462@qq.com>
2021-08-09 23:56:53 +0800

    windows:三数之和

---
+++ TwoPoint/threeSum/threeSum_test.go
@@ -0,0 +1,76 @@
+package threeSum
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+/*
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+
+
+示例 1：
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+
+示例 2：
+输入：nums = []
+输出：[]
+
+示例 3：
+输入：nums = [0]
+输出：[]
+
+
+作者：力扣 (LeetCode)
+链接：https://leetcode-cn.com/leetbook/read/sliding-window-and-two-pointers/odgk82/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	// 枚举 a
+	for first := 0; first < n; first++ {
+		// 需要和上一次枚举的数不相同
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// c 对应的指针初始指向数组的最右端
+		third := n - 1
+		target := -1 * nums[first]
+		// 枚举 b
+		for second := first + 1; second < n; second++ {
+			// 需要和上一次枚举的数不相同
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			// 需要保证 b 的指针在 c 的指针的左侧
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			// 如果指针重合，随着 b 后续的增加
+			// 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+	return ans
+}
+
+func TestName(t *testing.T) {
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	fmt.Println(threeSum([]int{}))
+	fmt.Println(threeSum([]int{0}))
+}
