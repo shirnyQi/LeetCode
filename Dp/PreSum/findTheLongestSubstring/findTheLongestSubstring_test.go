69b9bdec11166544ca3f2a27ba0a83e97421b518
hanlinqi <34259132+shirnyQi@users.noreply.github.com>
2021-08-13 01:05:27 +0800

    dp presum:每个元音包含偶数次的最长子字符串

--- Dp/PreSum/findTheLongestSubstring/findTheLongestSubstring_test.go
+++ Dp/PreSum/findTheLongestSubstring/findTheLongestSubstring_test.go
@@ -6,40 +6,7 @@ import (
 )
 
 /*
-
-func findTheLongestSubstring(s string) int {
-    ans, status := 0, 0
-    pos := make([]int, 1 << 5)
-    for i := 0; i < len(pos); i++ {
-        pos[i] = -1
-    }
-    pos[0] = 0
-    for i := 0; i < len(s); i++ {
-        switch s[i] {
-        case 'a':
-            status ^= 1 << 0
-        case 'e':
-            status ^= 1 << 1
-        case 'i':
-            status ^= 1 << 2
-        case 'o':
-            status ^= 1 << 3
-        case 'u':
-            status ^= 1 << 4
-        }
-        if pos[status] >= 0 {
-            ans = Max(ans, i + 1 - pos[status])
-        } else {
-            pos[status] = i + 1
-        }
-    }
-    return ans
-}
-
-作者：力扣 (LeetCode)
-链接：https://leetcode-cn.com/leetbook/read/dynamic-programming-1-plus/5ms10i/?discussion=VpMzlB
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+每个元音包含偶数次的最长子字符串
 给你一个字符串 s ，请你返回满足以下条件的最长子字符串的长度：每个元音字母，即 'a'，'e'，'i'，'o'，'u' ，在子字符串中都恰好出现了偶数次。
 
 示例 1：
