19c83b286d8e690a21c226cc0f3785419ef9a87d
hanlinqi <34259132+shirnyQi@users.noreply.github.com>
2021-09-13 23:26:17 +0800

    twopoint: 满足不等式的最大值

--- TwoPoint/findMaxValueOfEquation/findMaxValueOfEquation_test.go
+++ TwoPoint/findMaxValueOfEquation/findMaxValueOfEquation_test.go
@@ -126,6 +126,7 @@ func findMaxValueOfEquation(points [][]int, k int) int {
 	return ret
 }
 
+
 func TestName(t *testing.T) {
 	fmt.Println(findMaxValueOfEquationWindows([][]int{{1, 3}, {2, 0}, {5, 10}, {6, -10}}, 1))
 	fmt.Println(findMaxValueOfEquationWindows([][]int{{0, 0}, {3, 0}, {9, 2}}, 3))
